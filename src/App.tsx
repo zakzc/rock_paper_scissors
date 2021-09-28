586eaa10af4f6b09769952226a9a94c1d600d429
zakzc <zak.zangrando@outlook.com>
2021-09-28 11:38:06 +0200

    Adds Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 //import logo from "./logo.svg";
 // style
+import React from "react";
 import "./app.scss";
 
-function App() {
+const App: React.FC = (): any => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rock Paper Scissors</h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
